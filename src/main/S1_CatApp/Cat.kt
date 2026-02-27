3a583aff6aa16ddcf80731a4d03acedd4fd62616
FlavioCunhaCode <prflaviocunha@gmail.com>
2026-02-27 15:46:54 +0000

    Commit 1

--- src/main/S1_CatApp/Cat.kt
+++ src/main/S1_CatApp/Cat.kt
@@ -1,3 +1,14 @@
 package S1_CatApp
 
-class Cat(val name: String, var age:)
+class Cat(val name: String, var age: Int, var weight:Int) {
+
+    fun walk() {
+        weight--
+    }
+    fun eat() {
+        weight++
+    }
+    override fun toString(): String {
+        return "Cat(name='$name', age=$age, weight=Sweight)"
+    }
+}
